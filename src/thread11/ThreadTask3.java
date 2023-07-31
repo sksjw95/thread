6047fa1049c37154e88e0506d4bc0b940bb6e0b3
sksjw95 <tjwldnd95@hanmail.net>
2023-08-01 01:02:48 +0900

    임계 영역 지정으로 문제해결

---
+++ src/thread11/ThreadTask3.java
@@ -0,0 +1,18 @@
+package thread11;
+
+public class ThreadTask3 implements  Runnable{
+    Account account = new Account();
+    public void run(){
+        while(account.getBalance() > 0){
+            // 100~300원의 인출금을 랜덤으로 정한다.
+            int money = (int)(Math.random()*3+1)*100;
+            // withdraw를 실행시키는 동시에 인출 성공 여부를 변수에 할당한다.
+            boolean denied = !account.withdraw(money);
+            // 인출 결과 확인
+            // 만약, withdraw가 false를 리턴하였다면, 즉 인출에 실패했다면,
+            // 해당 내역에 ->DENIED를 출력합니다.
+            System.out.println(String.format("withdraw %d\\ By %s. Balance : %d %s",
+                    money,Thread.currentThread().getName(),account.getBalance(),denied?"->DENIED":""));
+        }
+    }
+}
